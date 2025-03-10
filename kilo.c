861158c8cb7dce110b48f650e327e94b21f7cbe9
Macjja <macjjaofficial@outlook.com>
2025-03-10 12:54:01 -0400

    Fix setFeatureFlag so that it actually sets the feature of Auto Indenting

--- kilo.c
+++ kilo.c
@@ -596,12 +596,12 @@ void setFeatureFlag(struct editorFeatures* feature, char* flag, char* val) {
     if (digit > 1) return;
     digit = digit<<0;
     feature->flags = feature->flags | digit;
+  }
   if (strcmp(flag, "AUTO_INDENT") == 0) {
     int digit = val[0] - '0';
     if (digit > 1) return;
     digit = digit<<1;
     feature->flags = feature->flags | digit;
-    }
   }
   if (strcmp(flag, "AUTO_INDENT") == 0) {
     int digit = val[0] - '0';
